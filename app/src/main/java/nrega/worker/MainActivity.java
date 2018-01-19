e87ab72c39b335fbd4ebb646a6fd1f7e6f55603d
hkoolwal34 <hkoolwal34@gmail.com>
2018-01-19 17:11:32 +0530

    ``

--- app/src/main/java/nrega/worker/MainActivity.java
+++ app/src/main/java/nrega/worker/MainActivity.java
@@ -1,16 +1,23 @@
 package nrega.worker;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import nrega.worker.Fragments.Ejobcard_fragment;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActionBar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_ejobcard_fragment);
+//        do not forget to comment the above line and uncomment the below line
+//        setContentView(R.layout.activity_main);
 
-        //this is a comment
 
     }
 }
