67c61dec049245f2fa4c61628ae756bc231beec9
hkoolwal34 <hkoolwal34@gmail.com>
2018-01-23 16:56:21 +0530

    Added DemandFragment java file and xml

---
+++ app/src/main/java/nrega/worker/Fragments/DemandFragment.java
@@ -0,0 +1,30 @@
+package nrega.worker.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import nrega.worker.R;
+
+public class DemandFragment extends Fragment {
+    public DemandFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_demand, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
